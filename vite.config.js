6f6084714a7da1102b80fd9e3eb487d39dd67bd4
Johnny <tutktoto05@gmail.com>
2024-10-06 21:34:45 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'jhn611.github.io/raschet_com/',
+  base: '/raschet_com/',
   plugins: [vue()],
   resolve: {
     alias: {
