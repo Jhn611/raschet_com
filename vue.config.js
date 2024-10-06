8dc3f7e720f81e818f6732297ee5110935024e58
Johnny <tutktoto05@gmail.com>
2024-10-06 19:34:04 +0300

    change

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/raschet_com/' : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  publicPath: `/raschet_com/`
+})
